65253191a7ff7a4b8a4b387a99d02ed99a8d9ea8
Emils <emilsberzs@hotmail.com>
2023-02-02 15:20:08 +0000

    File structure and skeleton

--- book.js
+++ book.js
@@ -1,5 +1,7 @@
-//Create constructor function for book object 
+//Create empty array to store books
+let my_library = [];
 
+//Create constructor function for book object 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,6 +9,12 @@ function Book(title, author, pages, read) {
     this.read = read;
     this.info = `${title} by ${author}, ${pages} pages, ${read}.`
 }
+
+//Create function to add book to library
+function addBookToLibrary(book) {
+    my_library.push(book)
+}
+
 
 //Add function prototype to object
 Book.prototype.describe = function () { console.log(this.info) };
