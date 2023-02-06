8e6ba96c900f3d2433d69fb3be2f0bf815d739ad
Emils <emilsberzs@hotmail.com>
2023-02-06 10:36:48 +0000

    Displays all books in my library, single div single line for now

--- book.js
+++ book.js
@@ -21,8 +21,19 @@ function addBookToLibrary(book) {
 Book.prototype.describe = function () { console.log(this.info) };
 
 //Create an instance of a book
-const book = new Book('Bible', 'God', 1000, 'not read yet');
+const book = new Book('Bible', 'God', 1000, 'not read');
 
+//Create more instances of books
+let book1 = new Book('The Linux Command Line', 'William Shotts', 470, 'not read')
+let book2 = new Book('Python Crash Course', 'Eric Matthes', 506, 'read')
+let book3 = new Book('Think Like a Programmer', 'V. Anthon Spraul', 233, 'not read')
+
+
+//Add all 4 books to my_library
+addBookToLibrary(book)
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
 //Console log info of a book
 book.describe()
 
@@ -35,8 +46,12 @@ let card_div = document.createElement("div")
 //Add class to card_div
 card_div.classList.add("card")
 
-//Add book info to card div
-card_div.innerHTML = book.info
+//Add all books from my_library to card div
+my_library.forEach(element => {
+    card_div.innerHTML += element.info;
+
+});
+
 
 //Append new div to container
 container.appendChild(card_div)
